6bf312efb0f8ac0225b0ef1dfffb1886e3161b71
Phanttt <117937309+Phanttt@users.noreply.github.com>
2023-11-05 20:30:34 +0200

    changes in db models

---
+++ Migrations/20231105182408_MediaChanged.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Museum.Migrations
+{
+    /// <inheritdoc />
+    public partial class MediaChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_Roleid",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Audios");
+
+            migrationBuilder.DropTable(
+                name: "Documents");
+
+            migrationBuilder.DropTable(
+                name: "Videos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Roleid",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_Roleid",
+                table: "Users",
+                column: "Roleid",
+                principalTable: "Roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_Roleid",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Roleid",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Audios",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MediaId = table.Column<int>(type: "int", nullable: false),
+                    data = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    note = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audios", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Audios_Medias_MediaId",
+                        column: x => x.MediaId,
+                        principalTable: "Medias",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MediaId = table.Column<int>(type: "int", nullable: false),
+                    data = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    note = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Documents_Medias_MediaId",
+                        column: x => x.MediaId,
+                        principalTable: "Medias",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Videos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MediaId = table.Column<int>(type: "int", nullable: false),
+                    data = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    note = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Videos_Medias_MediaId",
+                        column: x => x.MediaId,
+                        principalTable: "Medias",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Audios_MediaId",
+                table: "Audios",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_MediaId",
+                table: "Documents",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videos_MediaId",
+                table: "Videos",
+                column: "MediaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_Roleid",
+                table: "Users",
+                column: "Roleid",
+                principalTable: "Roles",
+                principalColumn: "id");
+        }
+    }
+}
---
+++ Migrations/20231105182849_EditsDeleted.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Museum.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditsDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EditInsideInfo");
+
+            migrationBuilder.DropTable(
+                name: "Edits");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Edits",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    itemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Edits", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EditInsideInfo",
+                columns: table => new
+                {
+                    editsid = table.Column<int>(type: "int", nullable: false),
+                    editsid1 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EditInsideInfo", x => new { x.editsid, x.editsid1 });
+                    table.ForeignKey(
+                        name: "FK_EditInsideInfo_Edits_editsid",
+                        column: x => x.editsid,
+                        principalTable: "Edits",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EditInsideInfo_InsideInfos_editsid1",
+                        column: x => x.editsid1,
+                        principalTable: "InsideInfos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EditInsideInfo_editsid1",
+                table: "EditInsideInfo",
+                column: "editsid1");
+        }
+    }
+}
